7dc35becfeb7504f3a74ac960bb1b7d1624908d9
Austin Zumbro <austin@Austins-MacBook-Pro.local>
2023-06-03 07:50:27 -0700

    schemas begun

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,10 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        library: 
+    }

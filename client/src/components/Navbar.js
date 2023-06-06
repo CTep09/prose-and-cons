7b7674c9332f258c70d88ca642ead77be59e608a
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-06-06 15:23:10 -0700

    added links to signup and sign in for navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -81,7 +81,7 @@ export default function WithSubnavigation() {
             fontSize={"sm"}
             fontWeight={400}
             variant={"link"}
-            href={"#"}
+            href={"/"}
           >
             Sign In
           </Button>
@@ -92,7 +92,7 @@ export default function WithSubnavigation() {
             fontWeight={600}
             color={"white"}
             bg={"pink.400"}
-            href={"#"}
+            href={"/signup"}
             _hover={{
               bg: "pink.300",
             }}

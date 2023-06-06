dbad498b84cbe858c64e9dc265e298848d84ff6c
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-06-06 11:35:44 -0700

    fixed conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Navbar from "./components/Navbar";
 
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
+        <Navbar />
         <h1>Hello World.</h1>
         <p>This is Prose and Cons.</p>
       </div>

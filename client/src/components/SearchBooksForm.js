5e68e4e0be31beab1660a7eccbf32a1c3ace256c
bdalberson <Brian.alberson@gmail.com>
2023-06-08 13:22:40 -0700

    Added the search to cards

--- client/src/components/SearchBooksForm.js
+++ client/src/components/SearchBooksForm.js
@@ -1,26 +1,9 @@
 import React, { useState } from "react";
-import {
-  Button,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  ModalFooter,
-  useDisclosure,
-  Flex,
-  Icon,
-  Text,
-  InputRightElement,
-} from "@chakra-ui/react";
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Flex, Icon, Text } from "@chakra-ui/react";
 import { searchGoogleBooks } from "../utils/api";
 import { useMutation } from "@apollo/client";
 import { AddIcon, Search2Icon } from "@chakra-ui/icons";
+import BookCard from "./BookCard";
 
 const SearchBooksForm = () => {
   // create state for holding returned google api data
@@ -49,7 +32,7 @@ const SearchBooksForm = () => {
         authors: book.volumeInfo.authors || [],
         title: book.volumeInfo.title,
         description: book.volumeInfo.description,
-        image: book.volumeInfo.imageLinks?.thumbnail || "",
+        image: book.volumeInfo.imageLinks?.smallThumbnail || "",
         num_pages: book.volumeInfo.pageCount,
         date_pub: book.volumeInfo.publishedDate,
       }));
@@ -63,43 +46,39 @@ const SearchBooksForm = () => {
 
   return (
     <>
-    <form onSubmit={handleFormSubmit}>
-      <FormControl>
-        <FormLabel>Search by ...</FormLabel>
-        <InputGroup>
-          <Input
-            placeholder="Book title"
-            name="searchInput"
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
-            type="text"
-          />
-          <InputRightElement>
-            <Search2Icon />
-          </InputRightElement>
-        </InputGroup>
-        <Button type="submit">Search</Button>
-      </FormControl>
+      <form onSubmit={handleFormSubmit}>
+        <FormControl>
+          <FormLabel>Search by ...</FormLabel>
+          <InputGroup>
+            <Input
+              placeholder="Book title"
+              name="searchInput"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              type="text"
+            />
+            <InputRightElement>
+              <Search2Icon />
+            </InputRightElement>
+          </InputGroup>
+          <Button type="submit">Search</Button>
+        </FormControl>
       </form>
       <Flex>
-         
         <div>
-          {searchedBooks.map((book) => {
-            return (
-              <div key={book.bookId}>
-                <h2>{book.title}</h2>
-                {book.authors ? <p>{ book.authors.join(", ")}</p> : <p>""</p>}
-                {book.date_pub}
-                    
-
-              </div>
-            );
-          })}
+          {searchedBooks.map((book) => (
+            <BookCard
+              key={book.bookId}
+              title={book.title}
+              author={book.authors.join(", ")}
+              img={book.image}
+              review={book.date_pub}
+            />
+          ))}
         </div>
       </Flex>
     </>
   );
 };
 
-
-export default SearchBooksForm
+export default SearchBooksForm;

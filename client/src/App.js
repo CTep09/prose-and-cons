135021ff22b2ae338a6e18a3dc4479dd69b14563
Austin Zumbro <azumbro@gmail.com>
2023-06-09 14:49:26 -0700

    switch createHttpLink uri back to the correct setting for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -29,8 +29,8 @@ import { ChakraProvider, Text, Link } from "@chakra-ui/react";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
-  // uri: "/graphql",
+  //uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header

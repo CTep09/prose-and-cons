d132b4a2044173f7556185fc8d9f832abe0d32e5
Austin Zumbro <azumbro@gmail.com>
2023-06-10 11:19:06 -0700

    file restructuring

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -10,38 +10,38 @@ import {
   Flex,
 } from "@chakra-ui/react";
 // import { Flex, Circle, Badge, Icon, chakra, Tooltip } from "@chakra-ui/react";
-import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
-import StarRating from "./StarRating";
-import ReadStatus from "./ReadStatus";
+// import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
+import StarRating from "./widgets/StarRating";
+import ReadStatus from "./widgets/ReadStatus";
 
 export default function BookCard(props) {
-  function Rating({ rating, numReviews }) {
-    return (
-      <Flex alignItems="center">
-        {Array(5)
-          .fill("")
-          .map((_, i) => {
-            const roundedRating = Math.round(rating * 2) / 2;
-            if (roundedRating - i >= 1) {
-              return (
-                <BsStarFill
-                  key={i}
-                  style={{ marginLeft: "1" }}
-                  color={i < rating ? "teal.500" : "gray.300"}
-                />
-              );
-            }
-            if (roundedRating - i === 0.5) {
-              return <BsStarHalf key={i} style={{ marginLeft: "1" }} />;
-            }
-            return <BsStar key={i} style={{ marginLeft: "1" }} />;
-          })}
-        <Box as="span" ml="2" color="gray.600" fontSize="sm">
-          {numReviews} review{numReviews > 1 && "s"}
-        </Box>
-      </Flex>
-    );
-  }
+  // function Rating({ rating, numReviews }) {
+  //   return (
+  //     <Flex alignItems="center">
+  //       {Array(5)
+  //         .fill("")
+  //         .map((_, i) => {
+  //           const roundedRating = Math.round(rating * 2) / 2;
+  //           if (roundedRating - i >= 1) {
+  //             return (
+  //               <BsStarFill
+  //                 key={i}
+  //                 style={{ marginLeft: "1" }}
+  //                 color={i < rating ? "teal.500" : "gray.300"}
+  //               />
+  //             );
+  //           }
+  //           if (roundedRating - i === 0.5) {
+  //             return <BsStarHalf key={i} style={{ marginLeft: "1" }} />;
+  //           }
+  //           return <BsStar key={i} style={{ marginLeft: "1" }} />;
+  //         })}
+  //       <Box as="span" ml="2" color="gray.600" fontSize="sm">
+  //         {numReviews} review{numReviews > 1 && "s"}
+  //       </Box>
+  //     </Flex>
+  //   );
+  // }
 
   return (
     <Center py={12}>

f93c08233f0aa964958f38a98d2c43300e4bac01
Cassie T <cassandray9@gmail.com>
2023-06-20 12:19:22 -0700

    Added conditional logic to useUser hook to solve error for non-signed in visitors

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -1,4 +1,5 @@
 import { useQuery } from "@apollo/client";
+import { useEffect } from "react";
 import Auth from "../utils/auth";
 import { useNavigate } from "react-router-dom";
 import { QUERY_ME } from "../utils/queries";
@@ -6,12 +7,16 @@ import { QUERY_ME } from "../utils/queries";
 const useUser = () => {
   const navigate = useNavigate();
 
-  const { loading, error, data } = useQuery(QUERY_ME);
+  // const { loading, error, data } = useQuery(QUERY_ME);
+  const { loading, error, data } = useQuery(QUERY_ME, {
+    skip: !Auth.loggedIn(),
+  });
 
-  if (!loading && !Auth.loggedIn()) {
-    navigate("/login");
-    return null;
-  }
+  useEffect(() => {
+    if (!loading && !Auth.loggedIn()) {
+      navigate("/login");
+    }
+  }, [loading]);
 
   return { loading, error, user: data?.me };
 };
